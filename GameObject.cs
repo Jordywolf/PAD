fef17ff53bbb58a7c398d924513da841232df03c
Nick Baptist <nick.baptist@hva.nl>
2021-02-22 16:27:20 +0100

    Environment, states en Objects

--- GameObject.cs
+++ GameObject.cs
@@ -1,38 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-using Microsoft.Xna.Framework;
+﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace BaseProject
+class GameObject
 {
-    class GameObject
+    public Vector2 position;
+    public Vector2 velocity;
+    public Texture2D texture;
+
+    public GameObject(String assetName)
     {
-        public Texture2D texture;
-        public Vector2 position;
-        public Vector2 velocity;
-
-        public GameObject(Texture2D objTexture)
-        {
-            texture = objTexture;
-            Reset();
-        }
-
-        virtual public void Reset()
-        {
-
-        }
-
-        virtual public void Update()
-        {
-
-        }
-
-        virtual public void Draw()
-        {
-
-        }
+        texture = GameEnvironment.ContentManager.Load<Texture2D>(assetName);
+        Reset();
     }
+
+    public virtual void Update() { }
+
+    public virtual void Draw(SpriteBatch spriteBatch)
+    {
+        spriteBatch.Draw(texture, position, Color.White);
+    }
+
+    public virtual void Reset()
+    {
+
+    }
+
+    public Boolean Overlaps(GameObject other)
+    {
+        float w0 = this.texture.Width,
+            h0 = this.texture.Height,
+            w1 = other.texture.Width,
+            h1 = other.texture.Height,
+            x0 = this.position.X,
+            y0 = this.position.Y,
+            x1 = other.position.X,
+            y1 = other.position.Y;
+
+        return !(x0 > x1 + w1 || x0 + w0 < x1 ||
+          y0 > y1 + h1 || y0 + h0 < y1);
+    }
+
 }
+
