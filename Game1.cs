21d07cfe0d44e6190b9ca2fab048eb80327f1690
Nidal Toufik <nidal.toufik@hva.nl>
2021-02-22 13:40:50 +0100

    TEST WEGGEHAALD IN GAME1.CS LINE 37!

--- Game1.cs
+++ Game1.cs
@@ -34,7 +34,7 @@ namespace BaseProject
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
-            //TEST
+            
 
             // TODO: Add your update logic here
 
