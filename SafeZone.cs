e180af1587ecb8a7b17efd7ee610f4a1bfae3d25
Nidal Toufik <nidal.toufik@hva.nl>
2021-02-23 15:04:41 +0100

    SafeZone geupdate

--- SafeZone.cs
+++ SafeZone.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 public class Safezone
 {
