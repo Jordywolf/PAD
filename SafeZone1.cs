468611ef98afb62494739196b313f98f2bc76fd7
Nidal Toufik <nidal.toufik@hva.nl>
2021-03-01 20:37:35 +0100

    ja

--- SafeZone1.cs
+++ SafeZone1.cs
@@ -14,6 +14,7 @@ namespace BaseProject
         //private List<Sprite> _sprites;
         public int height = 1080;
         public int width = 1920;
+        public Boolean GameState3;
         public Boolean KeyCollected = true;
         public Vector2 SteenPosition,SteenVertPosition = new Vector2(0, 0);
         public Vector2 PlayerPosition = new Vector2(1920/2, 1080);
@@ -23,26 +24,11 @@ namespace BaseProject
         public Vector2 DoorPosition = new Vector2(1920 / 2, 1080 / 100);
         //public Texture2D FonteinTexture, Pilaar, SteenTile, ZandTile, SteenVert, Boom, Rots, Deur, Player, Sleutel;
         
-        /*protected override void LoadContent()
-        {
-            spriteBatch = new SpriteBatch(GraphicsDevice);
-            Pilaar = Content.Load<Texture2D>("Pilaar");
-            FonteinTexture = Content.Load<Texture2D>("Fontein");
-            ZandTile = Content.Load<Texture2D>("ZandTile");
-            SteenTile = Content.Load<Texture2D>("steen");
-            SteenVert = Content.Load<Texture2D>("SteenVert");
-            Boom = Content.Load<Texture2D>("Boom");
-            Rots = Content.Load<Texture2D>("Rots");
-            Deur = Content.Load<Texture2D>("Deur");
-            Player = Content.Load<Texture2D>("Player");
-            Sleutel = Content.Load<Texture2D>("Sleutel");
-
-            // TODO: use this.Content to load your game content here
-        }*/
         public  void NextLevel1()
         {
             if (PlayerPosition.Y > DoorPosition.Y && KeyCollected == true && Keyboard.GetState().IsKeyDown(Keys.Space))
             {
+                //Volgende gamestate
                 Console.WriteLine(PlayerPosition);
                 Exit();
             }
@@ -50,7 +36,7 @@ namespace BaseProject
             {
 
 
-                KeyCollected = true;
+                KeyCollected = false;
             }
         }
         public void SafeZoneStone(Texture2D SteenTile, SpriteBatch spriteBatch)
