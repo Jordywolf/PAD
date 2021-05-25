91b89b515703429cc827779851b55d5663c61bc6
Nidal Toufik <nidal.toufik@hva.nl>
2021-05-25 13:33:37 +0200

    Fixed collsion safezone2

--- GameStates/SafeZoneState2.cs
+++ GameStates/SafeZoneState2.cs
@@ -13,19 +13,19 @@ namespace BaseProject.GameStates
     class SafeZoneState2 : Engine.LevelPlayingState
     {
 
-        SpriteGameObject Fontein2, deur2, dropZone1,dropZone2,dropZone3,dropZone4;
+        SpriteGameObject Fontein2, deur2, dropZone1, dropZone2, dropZone3, dropZone4, fonteinCenter;
         public Vector2 deurPos = new Vector2(1000, 0);
         public Vector2 TileSz2Pos = new Vector2(0, 0);
         public int LowerPosY = 570;
         public int PlatformPosY;
 
-        private bool playerSpawned;
+        Player player1;
 
-        RotatingSpriteGameObject transition;
+        private bool playerSpawned;
 
         public SafeZoneState2() : base()
         {
-            
+
             PlatformPosY = 380;
             //Upper Row
             //Lower Row
@@ -35,7 +35,7 @@ namespace BaseProject.GameStates
                 LoadSquareFloor("TileSz2", TileSz2Pos.Y, TileSz2Pos.X, new Vector2(80 * i, LowerPosY));
             }
             //PlatForm
-            for(int i = 0; i < 5; i++)
+            for (int i = 0; i < 5; i++)
             {
                 LoadSquareFloor("TileSz2", TileSz2Pos.Y, TileSz2Pos.X, new Vector2(400 + (80 * i), PlatformPosY));
                 LoadSquareFloor("TileSz2", TileSz2Pos.Y, TileSz2Pos.X, new Vector2(400 + (80 * i), PlatformPosY - 70));
@@ -50,27 +50,27 @@ namespace BaseProject.GameStates
             gameObjects.AddChild(Game1.playerHealth1);
             gameObjects.AddChild(Game1.playerHealth2);
             gameObjects.AddChild(Game1.playerHealth3);
-            
-            gameObjects.AddChild(Game1.player);
-            Game1.player.LocalPosition = new Vector2(Game1.width/2 , Game1.height - 50);
-            deur2 = new ObjectTile("Deur", new Vector2(0,0),1);
+
+
+            player1 = Game1.player;
+
+
+            gameObjects.AddChild(player1);
+            player1.LocalPosition = new Vector2(Game1.width / 2, Game1.height - 50);
+            fonteinCenter = new ObjectTile("Blok3", new Vector2(0, 0), 1);
+            deur2 = new ObjectTile("Deur", new Vector2(0, 0), 1);
             walls.AddChild(deur2);
             deur2.LocalPosition = deurPos;
             deur2.scale = 0.9f;
             Fontein2 = new SpriteGameObject("Fontein", 1);
             gameObjects.AddChild(Fontein2);
-            
+            walls.AddChild(fonteinCenter);
+
             Fontein2.scale = 0.6f;
             Fontein2.LocalPosition = new Vector2(Game1.width / 2 - 200, 200);
-            
-
-            transition = new RotatingSpriteGameObject("Selin_small", 1);
-            transition.LocalPosition = new Vector2(Game1.width / 2, Game1.height / 2);
-            transition.Origin = transition.sprite.Center;
-            transition.scale = 60;
-
-            gameObjects.AddChild(transition);
-
+            fonteinCenter.LocalPosition = new Vector2(Fontein2.LocalPosition.X + 55, Fontein2.LocalPosition.Y + 40);
+            fonteinCenter.scale = 0.5f;
+            fonteinCenter.Visible = false;
             dropZone1 = new SpriteGameObject("Blok", 1);
             dropZone2 = new SpriteGameObject("Blok", 1);
             dropZone3 = new SpriteGameObject("Blok2", 1);
@@ -79,28 +79,22 @@ namespace BaseProject.GameStates
             dropZone1.scale = 0.3f;
             dropZone2.scale = 0.3f;
             dropZone3.scale = 0.3f;
-            dropZone1.LocalPosition = new Vector2(Game1.width  - 350, Game1.height - 150);
-            dropZone2.LocalPosition = new Vector2(Game1.width  -700, Game1.height - 520);
-            dropZone3.LocalPosition = new Vector2(Game1.width  - 1050, Game1.height - 250);
+            dropZone1.LocalPosition = new Vector2(Game1.width - 350, Game1.height - 150);
+            dropZone2.LocalPosition = new Vector2(Game1.width - 700, Game1.height - 520);
+            dropZone3.LocalPosition = new Vector2(Game1.width - 1050, Game1.height - 250);
             dropZone4.LocalPosition = new Vector2(Game1.width - 280, Game1.height - 250);
         }
 
         public override void Update(GameTime gameTime)
         {
             base.Update(gameTime);
-
-            transition.scale -= 1;
-            transition.Angle += 5;
-
-            if (transition.scale <= 0) { transition.Visible = false; transition.scale = 1; }
-
-            CollisionUpdate(Game1.player);
-            if (OverlapsWith(Game1.player, Fontein2))
+            CollisionUpdate(player1);
+            if (OverlapsWith(player1, Fontein2))
             {
                 gameObjects.AddChild(Game1.playerHealth1);
                 gameObjects.AddChild(Game1.playerHealth2);
                 gameObjects.AddChild(Game1.playerHealth3);
-                Game1.player.health = 3;
+                player1.health = 3;
 
             }
 
@@ -110,32 +104,33 @@ namespace BaseProject.GameStates
                 playerSpawned = true;
             }
 
-            if (OverlapsWith(Game1.player, deur2) && Keyboard.GetState().IsKeyDown(Keys.Space)) {
+            if (OverlapsWith(Game1.player, deur2) && Keyboard.GetState().IsKeyDown(Keys.Space))
+            {
                 Game1.GameStateManager.SwitchTo("safeZoneState2", "selinLevelPlayingState", new GameStates.SelinLevelPlayingState());
                 Game1.GameStateManager.SwitchTo("selinLevelPlayingState", "safeZoneState2", new GameStates.SafeZoneState2());
                 Game1.SelinScream.Play();
-                Game1.player.LocalPosition = new Vector2(Game1.width / 2, Game1.height / 2);
+                player1.LocalPosition = new Vector2(Game1.width / 2, Game1.height / 2);
 
             }
-            if (OverlapsWith(Game1.player, dropZone1) && ActionJump.playerOnGround == true)
+            if (OverlapsWith(player1, dropZone1) && ActionJump.playerOnGround == true)
             {
                 //Game1.GameStateManager.SwitchTo("deathState", "safeZoneState", new GameStates.SafeZoneState());
-                Game1.player.LocalPosition = new Vector2(Game1.width / 2, Game1.height - 20);
+                player1.LocalPosition = new Vector2(Game1.width / 2, Game1.height - 20);
             }
-            if (OverlapsWith(Game1.player, dropZone2) && ActionJump.playerOnGround == true)
+            if (OverlapsWith(player1, dropZone2) && ActionJump.playerOnGround == true)
             {
                 //Game1.GameStateManager.SwitchTo("deathState", "safeZoneState", new GameStates.SafeZoneState());
-                Game1.player.LocalPosition = new Vector2(Game1.width / 2, Game1.height - 20);
+                player1.LocalPosition = new Vector2(Game1.width / 2, Game1.height - 20);
             }
-            if (OverlapsWith(Game1.player, dropZone3) && ActionJump.playerOnGround == true)
+            if (OverlapsWith(player1, dropZone3) && ActionJump.playerOnGround == true)
             {
                 // Game1.GameStateManager.SwitchTo("deathState", "safeZoneState", new GameStates.SafeZoneState());
-                Game1.player.LocalPosition = new Vector2(Game1.width / 2, Game1.height - 20);
+                player1.LocalPosition = new Vector2(Game1.width / 2, Game1.height - 20);
             }
-            if (OverlapsWith(Game1.player, dropZone4) && ActionJump.playerOnGround == true)
+            if (OverlapsWith(player1, dropZone4) && ActionJump.playerOnGround == true)
             {
                 // Game1.GameStateManager.SwitchTo("deathState", "safeZoneState", new GameStates.SafeZoneState());
-                Game1.player.LocalPosition = new Vector2(Game1.width / 2, Game1.height - 20);
+                player1.LocalPosition = new Vector2(Game1.width / 2, Game1.height - 20);
             }
         }
     }
